c8cd084edf1f5f0f1d37a0daeb0bc55ce7eb9e09
ojela028xamk <jerelans91@gmail.com>
2025-04-02 12:55:01 +0300

    shadcn library added

--- src/app/Components/DictionarySearch.tsx
+++ src/app/Components/DictionarySearch.tsx
@@ -1,20 +1,19 @@
-import { Dispatch, SetStateAction } from "react";
-import css from "./DictionarySearch.module.scss";
+import { Dispatch, SetStateAction } from 'react'
+import { Input } from '@/components/ui/input'
 
 type DictionarySearchProps = {
-  setSearchWord: Dispatch<SetStateAction<string>>;
-};
+  setSearchWord: Dispatch<SetStateAction<string>>
+}
 
 const DictionarySearch = ({ setSearchWord }: DictionarySearchProps) => {
   return (
-    <div className={css.dictionary_search}>
-      <input
-        type="text"
-        placeholder="Hae..."
-        onChange={(event) => setSearchWord(event.currentTarget.value)}
-      />
-    </div>
-  );
-};
+    <Input
+      className="text-[clamp(1em, 5vw, 1.5em)] mx-[auto] my-[1em] w-[90%] rounded-[0.4em] border-none bg-[black] p-6 pt-7 text-[white] [box-shadow:3px_3px_0px_orange] placeholder:text-2xl"
+      type="text"
+      placeholder="Hae..."
+      onChange={(event) => setSearchWord(event.currentTarget.value)}
+    />
+  )
+}
 
-export default DictionarySearch;
+export default DictionarySearch
