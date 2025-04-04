e49f88b585f5c5067974970a697c709fa38fd8b7
ojela028xamk <jerelans91@gmail.com>
2025-04-04 15:58:38 +0300

    search update

--- src/app/Components/DictionarySearch.tsx
+++ src/app/Components/DictionarySearch.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 import css from "./DictionarySearch.module.scss";
+import { IoIosSearch } from "react-icons/io";
 
 type DictionarySearchProps = {
   setSearchWord: Dispatch<SetStateAction<string>>;
@@ -8,7 +9,9 @@ type DictionarySearchProps = {
 const DictionarySearch = ({ setSearchWord }: DictionarySearchProps) => {
   return (
     <div className={css.dictionary_search}>
+      <IoIosSearch className={css.dictionary_search_icon} />
       <input
+        className={css.dictionary_search_input}
         type="text"
         placeholder="Hae..."
         onChange={(event) => setSearchWord(event.currentTarget.value)}
