61efb6972881348615045144e81671ccab8ef869
ojela028xamk <jerelans91@gmail.com>
2026-02-03 18:33:15 +0200

    remove images

--- src/app/Components/MusicBackground.tsx
+++ src/app/Components/MusicBackground.tsx
@@ -1,36 +1,9 @@
-// https://www.flaticon.com/free-icons/music-note
-import Image from "next/image";
 import css from "./MusicBackground.module.scss";
-import guitarist1 from "../../../public/guitarist1.svg";
-import guitarist2 from "../../../public/guitarist2.svg";
-import notes1 from "../../../public/notes1.png";
-import notes2 from "../../../public/notes2.png";
 
 const MusicBackground = () => {
   return (
     <div className={css.music_background}>
-      <div className={css.background}>
-        <Image
-          className={`${css.vector} ${css.vector_left}`}
-          src={guitarist1}
-          alt="Image of a guitarist"
-        />
-        <Image
-          className={`${css.vector} ${css.vector_right}`}
-          src={guitarist2}
-          alt="Image of a guitarist"
-        />
-        <Image
-          className={`${css.notes} ${css.notes_left}`}
-          src={notes1}
-          alt="Music Notes"
-        />
-        <Image
-          className={`${css.notes} ${css.notes_right}`}
-          src={notes2}
-          alt="Music Notes"
-        />
-      </div>
+      <div className={css.background}></div>
     </div>
   );
 };
